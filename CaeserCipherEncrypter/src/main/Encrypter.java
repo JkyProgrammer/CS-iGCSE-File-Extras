8dbc5a3db6ea22fe849b8b21878aa6d63779e9d2
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-12-21 15:43:00 +0000

    Things

--- CaeserCipherEncrypter/src/main/Encrypter.java
+++ CaeserCipherEncrypter/src/main/Encrypter.java
@@ -6,30 +6,71 @@ public class Encrypter {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner (System.in);
-		System.out.print("Enter plain text to encrypt: ");
+		System.out.print ("Enter 'e' to encrypt, or anything else to decrypt: ");
+		boolean isEncrypt = s.nextLine().equals("e");
+		
+		System.out.print("Enter plain text to encrypt/decrypt: ");
 		String input = s.nextLine();
 		System.out.print("Enter an integer to use as a shift key: ");
 		int inputKey = Integer.parseInt (s.nextLine());
 		
-		String cipherText = encrypt (input, inputKey);
-		System.out.println(cipherText);
+		if (isEncrypt) {
+			String cipherText = encrypt (input, inputKey);
+			System.out.println(cipherText);
+		} else {
+			String plainText = decrypt (input, inputKey);
+			System.out.println(plainText);
+		}
 	}
+	
+	
 	
 	public static String encrypt (String plainText, int shiftKey) {
 		String cipherText = "";
 		
 		for (char c : plainText.toCharArray()) {
-			if (c == '.' || c == ' ') {
-				cipherText += c;
-			} else {
-				int chr = (int) c;
-				chr += shiftKey;
-				char chrr = (char) chr;
-				cipherText += chrr;
-			}
+			cipherText += getChar (c, shiftKey);
+		}
+		
+		return cipherText;
+	}
+	
+	public static String decrypt (String plainText, int shiftKey) {
+		String cipherText = "";
+		
+		for (char c : plainText.toCharArray()) {
+			cipherText += getChar (c, -shiftKey);
 		}
 		
 		return cipherText;
 	}
 
+	private static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	
+	private static char getChar (char inChar, int shiftKey) {
+		char outChar = inChar;
+		String inString = String.valueOf(inChar);
+		
+		if (alphabet.contains(inString)) {
+			outChar += shiftKey;
+			if (inString.equals(inString.toLowerCase())) { // Was lowercase letter
+				while (outChar > 'z') {
+					outChar -= 26;
+				}
+				while (outChar < 'a') {
+					outChar += 26;
+				}
+			} else { // Was uppercase letter
+				while (outChar > 'Z') {
+					outChar -= 26;
+				}
+				while (outChar < 'A') {
+					outChar += 26;
+				}
+			}
+		}
+		
+		return outChar;
+	}
+	
 }
